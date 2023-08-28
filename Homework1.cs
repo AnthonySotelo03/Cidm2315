d253ad716fd5ee675d15c2dc01dd6250ee3f0e5a
AnthonySotelo03 <143431783+AnthonySotelo03@users.noreply.github.com>
2023-08-28 18:44:18 -0500

    Add files via upload

---
+++ Homework1.cs
@@ -0,0 +1,18 @@
+﻿using System.IO.Compression;
+
+namespace Homework1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        float x = 2.5f;
+        float y = 3.3f;
+
+        float z = 4 * x * x + 3 * y;
+
+        Console.WriteLine("X = 2.5,\tY = 3.3\nThe value of Z is: " + z);
+
+    }
+}
