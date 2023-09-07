a4680fe84dd81f040f4aed6d9fe0770748a0a511
AnthonySotelo03 <143431783+AnthonySotelo03@users.noreply.github.com>
2023-09-06 22:53:09 -0500

    Add files via upload

---
+++ Homework2Q1.cs
@@ -0,0 +1,37 @@
+﻿namespace Homework2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Insert input
+        Console.WriteLine("Please input a letter grade:");
+        char grade = Console.ReadLine().ToUpper()[0];
+        //To Convert string to uppercase
+
+
+        int gpa_points;
+
+        switch(grade){
+            case 'A':
+                gpa_points = 4;
+                break;
+            case 'B':
+                gpa_points = 3;
+                break;
+            case 'C':
+                gpa_points = 2;
+                break;
+            case 'D':
+                gpa_points = 1;
+                break;
+            case 'F':
+                gpa_points = 0;
+                break;
+            default:
+                Console.WriteLine("Wrong letter grade!");
+                return;
+        }
+            Console.WriteLine($"Gpa points: {gpa_points}");
+    }
+}
---
+++ Homework2Q2.cs
@@ -0,0 +1,33 @@
+﻿namespace Homework2Q2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Input the first num:");
+        double first_num = Convert.ToInt16(Console.ReadLine());
+
+        Console.WriteLine("Input the second num:");
+        double sec_num = Convert.ToInt16(Console.ReadLine());
+
+        Console.WriteLine("Input the third num:");
+        double third_num = Convert.ToInt16(Console.ReadLine());
+
+        if(first_num<sec_num){
+            if(first_num<third_num){
+                Console.WriteLine("The smallest value is: "+ first_num);
+            }
+            else{
+                Console.WriteLine("The smallest value is: " +third_num);
+            }
+        }
+        else{
+            if(sec_num<third_num){
+                Console.WriteLine("The smallest value is:" +sec_num);
+            }
+            else{
+                Console.WriteLine("The smallest value is: "+third_num);
+            }
+        }
+    }
+}
